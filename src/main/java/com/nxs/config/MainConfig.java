673507e17427d69aba95def6d47528b72971b70c
jonenxs <570141370@qq.com>
2018-06-27 22:45:02 +0800

    组件注册，自定义过滤规则

--- src/main/java/com/nxs/config/MainConfig.java
+++ src/main/java/com/nxs/config/MainConfig.java
@@ -1,6 +1,7 @@
 package com.nxs.config;
 
 import com.nxs.bean.Person;
+import com.nxs.service.BookService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,11 @@ import org.springframework.stereotype.Service;
  * @Configuration 告诉Spring这是一个配置类
  * @ComponentScan value 指定要扫描的包 excludeFilters 指定扫描时排除那些组件
  * includeFilters 指定扫描时只包含那些组件
+ * FilterType.ANNOTATION 按照注解
+ * FilterType.ASSIGNABLE_TYPE 按照给定的类型
+ * FilterType.ASPECTJ 按照ASPECTJ表达式
+ * FilterType.REGEX 按照REGEX表达式
+ * FilterType.CUSTOM 按照自定义规则
  **/
 @Configuration
 @ComponentScan(value = "com.nxs" ,
@@ -23,7 +29,9 @@ import org.springframework.stereotype.Service;
 //        @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = {Controller.class})
 //        },
         includeFilters = {
-        @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = {Service.class})
+//        @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = {Service.class}),
+//                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,classes = {BookService.class}),
+                @ComponentScan.Filter(type = FilterType.CUSTOM,classes = MyTypeFilter.class)
 }, useDefaultFilters = false)
 public class MainConfig {
 
